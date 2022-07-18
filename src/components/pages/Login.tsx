81ff4985dd021316a7fb8db5c51807526f930a5c
ky0613 <95160084+ky0613@users.noreply.github.com>
2022-07-18 17:31:19 +0900

    ログイン機能の実装

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,9 +1,17 @@
 import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
-import { VFC, memo } from "react";
+import { VFC, memo, useState, ChangeEvent } from "react";
+import { useAuth } from "../../hooks/useAuth";
 
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
+  const { loading, login } = useAuth();
+  const [userId, setUserId] = useState("");
+
+  const onChangeUserID = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value);
+  const onClickLogin = () => login(userId);
+
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -12,8 +20,18 @@ export const Login: VFC = memo(() => {
         </Heading>
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
-          <Input placeholder="ユーザーID" />
-          <PrimaryButton>ログイン</PrimaryButton>
+          <Input
+            placeholder="ユーザーID"
+            value={userId}
+            onChange={onChangeUserID}
+          />
+          <PrimaryButton
+            onClick={onClickLogin}
+            disabled={userId === ""}
+            loading={loading}
+          >
+            ログイン
+          </PrimaryButton>
         </Stack>
       </Box>
     </Flex>
